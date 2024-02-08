80b08500786dbe78def9f72a89f890e0dcdad59f
Will Birdsall <will@ignition72.com>
2024-02-07 23:41:20 -0500

    Add treesitter config

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,11 +1,26 @@
 require("lazy").setup({
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{ "ellisonleao/gruvbox.nvim" },
-    { "junegunn/fzf", build = "./install --all" }
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme gruvbox]])
+    end,
+  },
+  { "junegunn/fzf", build = "./install --all" }
 })
 
 require("gruvbox").setup({
 	italic = {
 		strings = false
 	}
+})
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {"c", "css", "html", "javascript", "lua", "markdown", "php", "rust", "toml" },
+  sync_install = false,
+  highlight = {
+    enable = true,
+  }
 })
